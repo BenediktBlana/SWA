d621220f0c10b2889a73395cea08ac3b01d80cca
Benedikt Blana <bblana61@gmail.com>
2020-10-04 19:47:41 +0200

    initial commit 2nd exercise

--- assigments/2/1.2/weather_report/static/main.js
+++ assigments/2/1.2/weather_report/static/main.js
@@ -1,12 +1,29 @@
 
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+var response = '';
 var request = new XMLHttpRequest();
 request.open('GET', 'http://localhost:8080/data')
 request.setRequestHeader('Content-Type', 'application/json')
 request.setRequestHeader('Accept', 'application/json')
 request.send()
 request.onload = () => {
-console.log(request.responseText)
+    response = JSON.parse(request.responseText)
+    console.log(getLatestMeasurementEachKind(response, 'Horsens'))
+
 }
 
- 
+function getLatestMeasurementEachKind(response, place, type) {
+    filteredByPlace = response.filter(obj => obj["place"] == place)
+    var result = [];
+
+    var lookup = {}
+    filteredByPlace.forEach(element => {
+        var type = element["type"]
+        if (!(type in lookup)) {
+            lookup[type] = 1;
+            result.push(type);
+        }
+
+    });
+}
